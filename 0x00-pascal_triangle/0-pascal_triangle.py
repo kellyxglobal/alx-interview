c67b752e7f54114cfae956652c5eb1cc30bb0777
kellyxglobal <kellyxglobal@gmail.com>
2023-03-26 21:30:57 +0000

    This code snippet defines a function named pascal_triangle that takes an integer n as an argument and returns a list of lists representing the first n rows of Pascal’s Triangle. If n is less than or equal to 0, the function returns an empty list. Otherwise, it initializes the first row of the triangle to [1] and then computes each subsequent row of the triangle by initializing a new row with the first element set to 1, computing each element of the row (except for the first and last elements) as the sum of the two elements above it, setting the last element of the row to 1, and appending the row to the triangle. Finally, it returns the completed triangle.

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -2,6 +2,8 @@
 """
 0-main
 """
+
+
 def pascal_triangle(n):
     if n <= 0:
         return []
@@ -11,18 +13,20 @@ def pascal_triangle(n):
 
     # Compute each row of the triangle
     for i in range(1, n):
-        # Initialize a new row with the first element set to 1
+        # Initialize a new row with
+        # the first element set to 1
         row = [1]
 
-        # Compute each element of the row (except for the first and last elements)
+        # Compute each element of the row
+        # (except for the 1st & last elements)
         for j in range(1, i):
             # Each element is the sum of the two elements above it
             element = triangle[i-1][j-1] + triangle[i-1][j]
             row.append(element)
 
-        # Set the last element of the row to 1 and append the row to the triangle
+        # Set the last element of the row to 1
+        # and append the row to the triangle
         row.append(1)
         triangle.append(row)
 
     return triangle
-
