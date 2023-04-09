523fe7697d66a66e45edd0cf39e8689651b11149
kellyxglobal <kellyxglobal@gmail.com>
2023-04-09 22:14:08 +0000

    This implementation uses the fact that the only two operations we can perform are "Copy All" and "Paste". We start with a single H character, and we can only "Paste" a number of times equal to the number of H characters we have at the moment. Therefore, the number of H characters in the file must be a product of primes.
    To find the prime factorization of n, we start by dividing n by 2 as many times as possible, then by 3 as many times as possible, then by 5, and so on, until we reach a prime factor. Each time we divide n by a prime factor, we add that factor to the number of operations.
    
    If n is impossible to achieve (i.e., if n is not a product of primes), the function returns 0.

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,34 +1,27 @@
 #!/usr/bin/python3
 
-
 def minOperations(n):
     """
-    Calculates the fewest number of operations needed
-    to result in exactly n H characters in the file.
+    Calculates the fewest number of operations needed to result in exactly n H characters in the file.
 
     Args:
-        n (int): The desired number of H characters in the file.
+    n (int): The number of H characters needed.
 
     Returns:
-        int: The fewest number of operations needed to achieve 
-	the desired number of H characters in the file,
-             or 0 if it is impossible to achieve the desired number.
+    int: The fewest number of operations needed to obtain n H characters. Returns 0 if n is impossible to achieve.
     """
-    if n == 1:
+
+    if n < 1:
         return 0
 
-    num_chars = 1
     operations = 0
+    factor = 2
 
-    while num_chars < n:
-        if n % num_chars == 0:
-            num_chars *= 2
-            operations += 2
+    while n > 1:
+        if n % factor == 0:
+            n //= factor
+            operations += factor
         else:
-            num_chars += 1
-            operations += 1
+            factor += 1
 
-    if num_chars == n:
-        return operations
-    else:
-        return 0
+    return operations
